05ab55fbce335f78f7dd16aed4a428d88dc7f250
Nataly Rocha <nata_r10@hotmail.com>
2020-11-25 14:51:28 -0500

    tested

---
+++ src/__mocks__/CustomerMock.js
@@ -0,0 +1,10 @@
+const CustomerMock = {
+    name: 'Nataly',
+    rentals: [
+        {
+            movieID: 0,
+            days: 3
+        }
+    ]
+}
+export default CustomerMock;
---
+++ src/__mocks__/MoviesMock.js
@@ -0,0 +1,7 @@
+const MoviesMock = [
+    {
+        code: 'new',
+        title: 'Titanic'
+    }
+]
+export default MoviesMock;
